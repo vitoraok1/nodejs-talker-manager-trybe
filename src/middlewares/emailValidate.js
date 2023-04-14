2a5f01fcc7471a9329af74da71827f65a90c8e2f
Vitor Aoki <vitoraoki17@gmail.com>
2023-04-14 16:43:59 -0300

    requirement 04 - fix lint

--- src/middlewares/emailValidate.js
+++ src/middlewares/emailValidate.js
@@ -2,13 +2,17 @@ const emailValidate = (req, res, next) => {
   const { email } = req.body;
   const emailRegex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
 
-  if (!email) return res.status(400)
-    .json({ message: 'O campo \"email\" é obrigatório' });
+  if (!email) {
+ return res.status(400)
+    .json({ message: 'O campo "email" é obrigatório' }); 
+}
 
-  if (!emailRegex.test(email)) return res.status(400)
-    .json({ message: 'O \"email\" deve ter o formato \"email@email.com\"' });
+  if (!emailRegex.test(email)) {
+ return res.status(400)
+    .json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+}
 
   return next();
-}
+};
 
 module.exports = emailValidate;
--- src/middlewares/passwordValidate.js
+++ src/middlewares/passwordValidate.js
@@ -1,13 +1,17 @@
 const passwordValidate = (req, res, next) => {
   const { password } = req.body;
 
-  if (!password) return res.status(400)
-    .json({ message: 'O campo \"password\" é obrigatório' });
+  if (!password) {
+ return res.status(400)
+    .json({ message: 'O campo "password" é obrigatório' }); 
+}
   
-  if (password.length < 6) return res.status(400)
-    .json({ message: 'O \"password\" deve ter pelo menos 6 caracteres' });
+  if (password.length < 6) {
+ return res.status(400)
+    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+}
 
   return next();
-}
+};
 
 module.exports = passwordValidate;
